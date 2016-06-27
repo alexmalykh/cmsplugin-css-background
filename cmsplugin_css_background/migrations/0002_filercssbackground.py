429fc478cc1c0a285f5cc89280a01fd7788335b8
Alex Malykh <a2m.dev@yandex.ru>
2016-06-27 18:05:46 +0600

    split initial migration into two

--- cmsplugin_css_background/migrations/0002_filercssbackground.py
+++ cmsplugin_css_background/migrations/0002_filercssbackground.py
@@ -2,17 +2,19 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import cms.models.pluginmodel
+import filer.fields.image
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('filer', '__first__'),
+        ('cmsplugin_css_background', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='CssBackground',
+            name='FilerCssBackground',
             fields=[
                 ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
                 ('color', models.CharField(default=b'', max_length=32, blank=True)),
@@ -20,7 +22,7 @@ class Migration(migrations.Migration):
                 ('attachment', models.CharField(default=b'scroll', max_length=8, choices=[(b'fixed', 'Fixed'), (b'scroll', 'Scrolling')])),
                 ('bg_position', models.CharField(default=b'0% 0%', max_length=24, verbose_name='Position', blank=True)),
                 ('forced', models.BooleanField(default=False, help_text='Mark CSS rules as important.')),
-                ('image', models.ImageField(upload_to=cms.models.pluginmodel.get_plugin_media_path)),
+                ('image', filer.fields.image.FilerImageField(to='filer.Image')),
             ],
             options={
                 'abstract': False,
