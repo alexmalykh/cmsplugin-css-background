a682b940ba0781d5bd3c7a3918fbc700c5ceeb00
Alex Malykh <a2m.dev@yandex.ru>
2016-06-27 16:45:06 +0600

    add classifiers and long_description to setup script

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 
+import io
 import setuptools
+
+
+with io.open('README.rst', 'r') as readme:
+    try:
+        long_description = readme.read()
+    except IOError:
+        long_description = ''
 
 
 setup_params = dict(
@@ -8,13 +16,27 @@ setup_params = dict(
     name='cmsplugin-css-background',
     use_scm_version=dict(root='.', relative_to=__file__),
     description='A django CMS plugin for managing CSS background styles',
+    long_description=long_description,
     license='MIT License',
     url='https://github.com/alexmalykh/cmsplugin-css-background',
     packages=setuptools.find_packages(),
-    include_package_data=True,
     install_requires=['django-cms>=3.3,<4.0', 'six'],
     setup_requires=['setuptools_scm'],
-    zip_safe=False
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
 
 
